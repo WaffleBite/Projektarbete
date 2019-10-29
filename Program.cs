ed4c1f582100163fc6637746ec214d76b4cb81e7
Karolina <karroliina.k@gmail.com>
2019-10-29 15:40:55 +0100

    Add menu and ability to exit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,48 @@ namespace Projektarbete
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                Console.Clear();
+
+                Console.WriteLine("1. Add coordinates");
+                Console.WriteLine("2. List attacks");
+                Console.WriteLine("3. The big red button");
+                Console.WriteLine("4. Exit");
+
+                ConsoleKeyInfo keyPressed = Console.ReadKey(true);
+
+                Console.Clear();
+
+                switch (keyPressed.Key)
+                {
+                    case ConsoleKey.D1:
+                    case ConsoleKey.NumPad1:
+
+                        break;
+
+                    case ConsoleKey.D2:
+                    case ConsoleKey.NumPad2:
+
+                        break;
+
+                    case ConsoleKey.D3:
+                    case ConsoleKey.NumPad3:
+
+                        break;
+
+                    case ConsoleKey.D4:
+                    case ConsoleKey.NumPad4:
+
+                        isRunning = false;
+
+                        break;
+                }
+            }
+
         }
     }
 }
